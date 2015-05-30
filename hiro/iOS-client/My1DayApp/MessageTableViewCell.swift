fecc898b83fe5cbb00a07512ac3c5591a70fed0f
fujii-h <fujii.hiroaki.fc9@is.naist.jp>
2015-05-30 14:51:59 +0900

    Mission 1-1 Modified by hiro

--- hiro/iOS-client/My1DayApp/MessageTableViewCell.swift
+++ hiro/iOS-client/My1DayApp/MessageTableViewCell.swift
@@ -12,16 +12,20 @@ class MessageTableViewCell: UITableViewCell {
     @IBOutlet weak private var iconImageView: UIImageView!
     @IBOutlet weak private var messageLabel: UILabel!
     // Mission1-1 UILabel のインスタンス変数を追加
+    @IBOutlet weak var created_at_label: UILabel!
     
     override func prepareForReuse() {
         self.iconImageView.image = nil
         self.messageLabel.text = nil
         // Mission1-1 UILabel のインスタンス変数を初期化
+        self.created_at_label.text = nil
+        
     }
     
     func setupComponentsWithMessage(message: Message) {
         self.iconImageView.image = message.icon
         self.messageLabel.text = message.body
         // Mission1-1 UILabel のインスタンス変数に created_at の値を代入
+        self.created_at_label.text = message.created_at
     }
 }
