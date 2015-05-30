16eafc99d05af81fdafb5ce36abe520e878ac4eb
fujii-h <fujii.hiroaki.fc9@is.naist.jp>
2015-05-30 17:02:06 +0900

    modified app.php

--- hiro/web-server/app.php
+++ hiro/web-server/app.php
@@ -23,6 +23,28 @@ $app->post('/messages', function (Request $request) use ($app) {
     $username = isset($data['username']) ? $data['username'] : '';
     $body = isset($data['body']) ? $data['body'] : '';
 
+    /* $createdMessage = $app->createMessage($username, $body, base64_encode(file_get_contents($app['icon_image_path']))); */
+
+/*    $createdMessage = $app->createMessage("bot", $body, base64_encode(file_get_contents($app['icon_image_path']))); */
+
+    $num = mt_rand(0,4);
+
+    if($num == 0){
+        $body = "Nobunaga Oda";
+    }else if($num == 1){
+        $body = "Naichinge-ru";
+    }else if($num == 2){
+        $body = "Zabieru";
+    }else if($num == 3){
+        $body = "Ainshutain";
+    }else if($num == 4){
+        $body = "Himiko";
+    }
+
+/*
+    if($username == "uranai"){
+    $createdMessage = $app->createMessage("Bot", "Daikichi", base64_encode(file_get_contents($app['icon_image_path'])));
+    }else{ */
     $createdMessage = $app->createMessage($username, $body, base64_encode(file_get_contents($app['icon_image_path'])));
 
     return $app->json($createdMessage);
